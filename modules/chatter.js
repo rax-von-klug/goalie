fec8083555b020bdd2932a80f69d124e8d129a8f
Christopher Lund <clund@connectionseducation.com>
2016-03-16 17:13:09 -0400

    modify poke method to use .say

--- modules/chatter.js
+++ modules/chatter.js
@@ -1,7 +1,7 @@
 var Chatter = function() {
 
     this.poke = function(bot) {
-        bot.api.chat.postMessage({
+        bot.say({
             text: ':stand_up: Stand up time! Update trello and vsts :stand_up:',
             channel: 'C0GB1N91V',
             as_user: true
